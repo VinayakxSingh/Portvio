2aa1aa5fb68d5b9e6beffc53fcc831e6b05d690d
Vinayak Singh <ishita.rs27@gmail.com>
2025-04-28 07:26:18 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
         {isLoggedIn ? (
           <>
             <Link to="/dashboard">Dashboard</Link>
-            <button className="logout-button" onClick={handleLogout}>
+            <button className="logout-button" onClick={handleLogout} style={{backgroundColor:"purple"}}>
               Logout
             </button>
           </>
